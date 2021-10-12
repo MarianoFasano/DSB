1d6b257b1ad0318da980fca3a9b2686783f83f6a
Mariano Fasano <mariano.fasano@student.supsi.ch>
2021-10-13 00:44:12 +0200

    first commit
    uses cases, mockup, wpf event test, thread test

---
+++ docs/CodeTest/HelloWorld_WPFTest/HelloWorld/SalutiTest.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cSharpeTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ThreadTest taskTest = new ThreadTest();
+        public MainWindow()
+        {
+            taskTest.ErrorEvent += ManageError;
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (HelloButton.IsChecked == true)
+            {
+                MessageBox.Show("Benvenuto/a!");
+            }
+            else if (GoodByeButton.IsChecked == true)
+            {
+                MessageBox.Show("Arrivederci!!");
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            taskTest.start();
+        }
+
+        public void ManageError(object sender, EventArgs args)
+        {
+            String number = "Data Manage TODO";
+            MessageBox.Show($"Error time {number}", "There is an error", MessageBoxButton.OK, MessageBoxImage.Warning, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
+        }
+    }
+}
---
+++ docs/CodeTest/HelloWorld_WPFTest/HelloWorld/ThreadTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace cSharpeTest
+{
+	public class ThreadTest
+	{
+		public ThreadTest()
+		{
+		}
+
+		//Delegate
+		public event EventHandler ErrorEvent;
+		public async void start()
+        {
+			//Task to simulate an error in DSB
+			//await Task.Run(() => doTask());
+
+			Thread t = new Thread(() => doTask());
+			t.Start();
+
+		}
+		private void doTask()
+        {
+			//If the countdown is a multiple of number, a new message on top is generated
+			Random random = new Random();
+			int number = random.Next(5, 18);
+
+			for (int i = 50; i > 5; i--)
+			{
+				if (i % number == 0)
+				{
+					errorEvent();
+				}
+				Thread.Sleep(300);
+			}
+		}
+
+		private void errorEvent()
+        {
+			if(ErrorEvent != null)
+            {
+				//Event
+				ErrorEvent(this, EventArgs.Empty);
+            }
+        }
+	}
+}
+
