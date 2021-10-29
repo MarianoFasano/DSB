f9e65099012167dfd8fe047d44f00cc19d7b31d0
Mariano Fasano <mariano.fasano@student.supsi.ch>
2021-10-29 14:34:37 +0200

    open list of depos from exps and refactoring

--- DataSetBuilder/DataSetBuilder/DSB_Controller.cs
+++ DataSetBuilder/DataSetBuilder/DSB_Controller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -58,6 +59,116 @@ namespace DataSetBuilder
             {
                 return "Nascondi Commento";
             }
+        }
+
+        //Create a new tabItem with TODO: initialization for depo view
+        internal TabItem NewDepTabItem(ListViewItem listViewItem)
+        {
+            TabItem tabItem = new TabItem { Header = listViewItem.Content };
+
+            Grid tabGrid = new Grid();
+
+            ColumnDefinition column1 = new ColumnDefinition();
+            column1.Width = new GridLength(1, GridUnitType.Star);
+            tabGrid.ColumnDefinitions.Add(column1);
+
+            ColumnDefinition column2 = new ColumnDefinition();
+            column2.Width = new GridLength(1, GridUnitType.Star);
+            tabGrid.ColumnDefinitions.Add(column2);
+
+            // Create a DockPanel    
+            DockPanel itemDock = new DockPanel();
+            // Create search text TextBox    
+            TextBox searchText = new TextBox();
+            searchText.Height = 20;
+            searchText.Text = "Cerca...";
+            searchText.TextWrapping = TextWrapping.Wrap;
+            // Dock TextBox to top    
+            DockPanel.SetDock(searchText, Dock.Top);
+            // Add docked button to DockPanel    
+            itemDock.Children.Add(searchText);
+
+
+            //Create Menu with MenuItem
+            Menu menu = createMenu("Edit");
+
+            MenuItem menuItem = createMenuItem("Edit");
+            var binding = new Binding("ActualWidth");
+            binding.Source = menu;
+            BindingOperations.SetBinding(menuItem, MenuItem.WidthProperty, binding);
+            menu.Items.Add(menuItem);
+
+            MenuItem menuItem1 = createMenuItem("Vedi commento");
+            menuItem.Items.Add(menuItem1);
+
+            MenuItem menuItem2 = createMenuItem("Directory");
+            menuItem.Items.Add(menuItem2);
+
+            MenuItem menuItem3 = createMenuItem("Cancella deposizione");
+            menuItem.Items.Add(menuItem3);
+            //Dock Menu to bottom
+            DockPanel.SetDock(menu, Dock.Bottom);
+            //Add docked Menu to DockPanel
+            itemDock.Children.Add(menu);
+
+            // Create comment text TextBox    
+            TextBox commentText = new TextBox();
+            commentText.Visibility = System.Windows.Visibility.Collapsed;
+            commentText.Height = Double.NaN;
+            // Dock TextBox to bottom    
+            DockPanel.SetDock(commentText, Dock.Bottom);
+            // Add docked TextBox to DockPanel    
+            itemDock.Children.Add(commentText);
+
+            // Create a ScrollView with nested StackPanel    
+            ScrollViewer depoView = new ScrollViewer();
+            StackPanel depoStack = initDepoList((string)listViewItem.Content);
+            //ListViewItem depoItem = new ListViewItem();
+
+            //depoItem.Content = "Deposition 2";
+            //depoStack.Children.Add(depoItem);
+            depoView.Content = depoStack;
+
+            // Dock ScrollView to left    
+            DockPanel.SetDock(depoView, Dock.Left);
+            // Add docked ScrollView to DockPanel    
+            itemDock.Children.Add(depoView);
+
+            Grid.SetColumn(itemDock, 0);
+            tabGrid.Children.Add(itemDock);
+
+            tabItem.Content = tabGrid;
+
+            return tabItem;
+        }
+
+        private Menu createMenu(String name)
+        {
+            Menu menu = new Menu();
+            menu.Name = name;
+            return menu;
+        }
+
+        private MenuItem createMenuItem(String name)
+        {
+            MenuItem menuItem = new MenuItem();
+            menuItem.Header = name;
+            return menuItem;
+        }
+
+        private StackPanel initDepoList(String subPath)
+        {
+            StackPanel depoList = new StackPanel();
+            string depoPath = @"D:\_DSB" + @"\" + subPath;
+            string[] depoDirectories = Directory.GetDirectories(depoPath);
+            for (int i = 0; i < depoDirectories.Length; i++)
+            {
+                var listItem = new ListViewItem();
+                listItem.Content = depoDirectories[i].Remove(0, depoPath.Length + 1);
+                //listItem.MouseDoubleClick += openExpDeps;
+                depoList.Children.Add(listItem);
+            }
+            return depoList;
         }
     }
 }
--- DataSetBuilder/DataSetBuilder/MainWindow.xaml.cs
+++ DataSetBuilder/DataSetBuilder/MainWindow.xaml.cs
@@ -32,7 +32,8 @@ namespace DataSetBuilder
         private void Init()
         {
             //TODO extra initialize
-            string expPath = @"J:\DTI\_DSB";
+            //string expPath = @"J:\DTI\_DSB";
+            string expPath = @"D:\_DSB";
             string[] expDirectories = Directory.GetDirectories(expPath);
             for(int i = 0; i < expDirectories.Length; i++)
             {
@@ -48,96 +49,7 @@ namespace DataSetBuilder
             ListViewItem listView = sender as ListViewItem;
             //MessageBox.Show("Hello, ciao", "There is an error", MessageBoxButton.OK, MessageBoxImage.Warning, MessageBoxResult.OK, MessageBoxOptions.DefaultDesktopOnly);
 
-            var tabItem = new TabItem { Header = listView.Content };
-
-            Grid tabGrid = new Grid();
-            tabGrid.Background = Brushes.Gray;
-
-            ColumnDefinition column1 = new ColumnDefinition();
-            column1.Width = new GridLength(1, GridUnitType.Star);
-            tabGrid.ColumnDefinitions.Add(column1);
-
-            ColumnDefinition column2 = new ColumnDefinition();
-            column2.Width = new GridLength(1, GridUnitType.Star);
-            tabGrid.ColumnDefinitions.Add(column2);
-
-            Label colonna1 = new Label();
-            colonna1.Content = "colonnA 1";
-
-            Label colonna2 = new Label();
-            colonna2.Content = "colonna 2";
-
-            //Grid.SetColumn(colonna1, 0);
-            //Grid.SetRow(colonna1, 0);
-            //Grid.SetColumn(colonna2, 1);
-            //Grid.SetRow(colonna2, 0);
-/*
-            //SearchBox
-            TextBox searchText = new TextBox();
-            searchText.Height = 20;
-            searchText.Text = "Cerca...";
-
-            //Menu
-            Menu menu = new Menu();
-            menu.Name = "Edit";
-            MenuItem menuItem = new MenuItem();
-            menuItem.Header = "Vedi commento";
-            menu.Items.Add(menuItem);
-
-            MenuItem menuItem1 = new MenuItem();
-            menuItem1.Header = "Directory";
-            menu.Items.Add(menuItem1);
-
-            MenuItem menuItem2 = new MenuItem();
-            menuItem2.Header = "Cancella deposizione";
-            menu.Items.Add(menuItem2);
-
-            //CommentBox
-            TextBox commentText = new TextBox();
-            commentText.Visibility = System.Windows.Visibility.Collapsed;
-            commentText.Height = Double.NaN;
-
-            //Viewer and panel
-            ScrollViewer depoView = new ScrollViewer();
-            StackPanel depoStack = new StackPanel();
-            ListViewItem depoItem = new ListViewItem();
-
-            depoItem.Content = "Deposition 2";
-            depoStack.Children.Add(depoItem);
-            depoView.Content = depoStack;
-
-            DockPanel itemDock = new DockPanel();
-            itemDock.MinWidth = 0;
-            itemDock.LastChildFill = true;
-            DockPanel.SetDock(searchText, Dock.Top);
-            DockPanel.SetDock(menu, Dock.Bottom);
-            DockPanel.SetDock(commentText, Dock.Bottom);
-            DockPanel.SetDock(depoView, Dock.Left);
-            DockPanel.SetDock(colonna1, Dock.Top);
-
-            Grid.SetColumn(itemDock, 0);
-            tabGrid.Children.Add(itemDock);
-*/
-            //tabGrid.Children.Add(colonna1);
-            //tabGrid.Children.Add(colonna2);
-
-            DockPanel itemDock = new DockPanel();
-            itemDock.Background = Brushes.LightCyan;
-
-            TextBox searchText = new TextBox();
-            searchText.Height = 20;
-            searchText.Text = "Cerca...";
-            searchText.TextWrapping = TextWrapping.Wrap;
-            searchText.Background = Brushes.Black;
-
-            DockPanel.SetDock(searchText, Dock.Left);
-
-            Grid.SetColumn(itemDock, 0);
-            tabGrid.Children.Add(itemDock);
-
-            tabItem.Content = tabGrid;
-
-            TabsControl.Items.Add(tabItem);
+            TabsControl.Items.Add(dsb_controller.NewDepTabItem(listView));
 
         }
 
